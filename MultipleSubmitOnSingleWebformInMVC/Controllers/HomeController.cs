cdb57cece73cfc2a0f99b810b432b1db8b52eb30
Aniketrana <ranaannu93@gmail.com>
2020-08-05 14:48:24 +0530

    Add project files.

---
+++ MultipleSubmitOnSingleWebformInMVC/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MultipleSubmitOnSingleWebformInMVC.Models;
+
+namespace MultipleSubmitOnSingleWebformInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Employee emp,string create,string edit)
+        {
+            /*3 ways to use multiple submit */
+
+            ///////////////////////
+
+            if (create == "create")
+            {
+                //Create code
+            }
+            if (edit == "edit")
+            {
+                //edit code
+            }
+            ///////////////////////
+            /*
+             if we use submit button as parameter
+            if (submit == "create")
+            {
+                //Create code
+            }
+            if (submit == "edit")
+            {
+                //edit code
+            }
+            */
+
+            ///////////////////////
+            
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee emp)
+        {
+            return RedirectToAction("Index");
+        }
+
+    }
+}
